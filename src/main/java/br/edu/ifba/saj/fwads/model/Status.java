bd102eac0bafeeff3446cce11467b9704f2e2f9f
Erik <20242TADSSAJ0008.ifba.edu.br>
2026-03-03 23:50:58 -0300

    Final da etapa 2. Criação da classe viagem para atender o req. 26 e 28 funcionais

---
+++ src/main/java/br/edu/ifba/saj/fwads/model/Status.java
@@ -0,0 +1,5 @@
+package br.edu.ifba.saj.fwads.model;
+
+public enum Status {
+        Resolvido, Processando, Pendente, Cancelada
+}
---
+++ src/main/java/br/edu/ifba/saj/fwads/model/Viagem.java
@@ -0,0 +1,66 @@
+package br.edu.ifba.saj.fwads.model;
+
+import java.time.LocalDateTime;
+
+public class Viagem {
+    private int id;
+    private Motorista motorista;
+    private Passageiro passageiro;
+    private Veiculo veiculo;
+    private String origem;
+    private String destino;
+    private LocalDateTime data;
+
+    public Viagem(int id, Motorista motorista, Passageiro passageiro, Veiculo veiculo, String origem, String destino, LocalDateTime data) {
+        this.id = id;
+        this.motorista = motorista;
+        this.passageiro = passageiro;
+        this.veiculo = veiculo;
+        this.origem = origem;
+        this.destino = destino;
+        this.data = data;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int novoId) {
+        this.id = novoId;
+    }
+    public Motorista getMotorista() {
+        return motorista;
+    }
+    public void setMotorista(Motorista novoMotorista) {
+        this.motorista = novoMotorista;
+    }
+    public Passageiro getPassageiro() {
+        return passageiro;
+    }
+    public void setPassageiro(Passageiro novoPassageiro) {
+        this.passageiro = novoPassageiro;
+    }
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+    public void setVeiculo(Veiculo novoVeiculo) {
+        this.veiculo = novoVeiculo;
+    }
+    public String getOrigem() {
+        return origem;
+    }
+    public void setOrigem(String novaOrigem) {
+        this.origem = novaOrigem;
+    }
+    public String getDestino() {
+        return destino;
+    }
+    public void setDestino(String novoDestino) {
+        this.destino = novoDestino;
+    }
+    public LocalDateTime getData() {
+        return data;
+    }
+    public void setData(LocalDateTime novaData) {
+        this.data = novaData;
+    }
+}
