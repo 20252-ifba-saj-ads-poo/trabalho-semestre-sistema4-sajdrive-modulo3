f5fcad735dc8c1f7ba525f610982e16f69039f3c
Erik <20242TADSSAJ0008.ifba.edu.br>
2026-03-24 23:07:20 -0300

    verificar o app

--- src/main/java/br/edu/ifba/saj/fwads/App.java
+++ src/main/java/br/edu/ifba/saj/fwads/App.java
@@ -1,7 +1,7 @@
 package br.edu.ifba.saj.fwads;
-
+ 
 import java.io.IOException;
-
+ 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,29 +9,32 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
-
+ 
 public class App extends Application {
     
     private static Scene scene;    
-
+ 
     @Override
 public void start(Stage stage) throws IOException {
+    // Para carregar a tela inicial do aplicativo
     FXMLLoader loader = new FXMLLoader(App.class.getResource("/br/edu/ifba/saj/fwads/controller/RealizarFeedback.fxml"));
     scene = new Scene(loader.load(), 800, 600);
     stage.setScene(scene);
     stage.show();
     }
-
+ 
 public static void setRoot(String fxml) {
     try {
         scene.setRoot(loadFXML(fxml));
     } catch (Exception e) {
+        // Para mostrar o erro para o usuário caso a tela não carregue
         new Alert(AlertType.ERROR, "Erro ao carregar o arquivo " + fxml).show();
         e.printStackTrace();
     }
     }
-
+ 
 private static Parent loadFXML(String fxml) throws Exception {
+    // Para carregar o arquivo FXML e associar o controller
     FXMLLoader loader = new FXMLLoader(App.class.getResource(fxml));
     Parent parent = loader.load();
     Object controller =loader.getController();
@@ -40,7 +43,7 @@ private static Parent loadFXML(String fxml) throws Exception {
     }
     return parent;
     }
-
+ 
 public static void main(String[] args) {
     launch();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module br.edu.ifba.saj.fwads {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.graphics;
+ 
+    opens br.edu.ifba.saj.fwads to javafx.graphics;
     opens br.edu.ifba.saj.fwads.controller to javafx.fxml;
     opens br.edu.ifba.saj.fwads.model to javafx.base, javafx.fxml;
-
+ 
     exports br.edu.ifba.saj.fwads;
     exports br.edu.ifba.saj.fwads.model;
     exports br.edu.ifba.saj.fwads.repository;
