0747ab9478a1ab8da439fa178bdff0e2258a9d73
Erik <20242TADSSAJ0008.ifba.edu.br>
2026-03-24 23:30:35 -0300

    teste

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module br.edu.ifba.saj.fwads {
     requires transitive javafx.graphics;
  
     opens br.edu.ifba.saj.fwads to javafx.graphics;
-    opens br.edu.ifba.saj.fwads.controller to javafx.fxml;
+    opens br.edu.ifba.saj.fwads.controller;
     opens br.edu.ifba.saj.fwads.model to javafx.base, javafx.fxml;
  
     exports br.edu.ifba.saj.fwads;
